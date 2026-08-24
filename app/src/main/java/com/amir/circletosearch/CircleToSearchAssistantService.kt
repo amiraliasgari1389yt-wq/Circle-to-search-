472e35438351aeba6435c45bd4fd2e900f10658b
amiraliasgari1389yt-wq <amirali.asgari.1389yt@gmail.com>
2026-08-24 23:38:40 +0330

    Create CircleToSearchAssistantService.kt

---
+++ app/src/main/java/com/amir/circletosearch/CircleToSearchAssistantService.kt
@@ -0,0 +1,32 @@
+package com.amir.circletosearch
+
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.service.voice.VoiceInteractionService
+
+class CircleToSearchAssistantService : VoiceInteractionService() {
+
+    private val prefsName = "circle_to_search"
+    private val targetPackageKey = "target_package"
+
+    override fun onReady() {
+        super.onReady()
+        launchTarget()
+    }
+
+    override fun onNewArgs(args: Bundle?) {
+        super.onNewArgs(args)
+        launchTarget()
+    }
+
+    private fun launchTarget() {
+        val prefs: SharedPreferences = getSharedPreferences(prefsName, MODE_PRIVATE)
+        val packageName = prefs.getString(targetPackageKey, null) ?: return
+
+        val launchIntent = packageManager.getLaunchIntentForPackage(packageName) ?: return
+
+        launchIntent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
+        startActivity(launchIntent)
+    }
+}
